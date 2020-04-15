f4800774d4d35953e2c6b7ed72d88370a263a654
Mohameth Seck <mohamethseck34@gmail.com>
2020-04-15 15:55:23 -0400

    added created components

---
+++ components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Heading from "./Heading"
+
+function App(){
+    return(
+        <Heading />
+    )
+};
+
+export default App;
---
+++ components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Note(){
+    return(
+        <div>
+            <h1>Title</h1>
+            <p>content</p>
+        </div>
+    )
+}
+
+export default Note;
